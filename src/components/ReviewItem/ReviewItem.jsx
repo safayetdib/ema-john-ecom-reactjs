2175a565e6ebd7bf53beee99fb2ce3febc7364e5
safayet <safayetdib@gmail.com>
2023-04-05 14:53:28 +0600

    added router and delete from cart

---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './ReviewItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
+const ReviewItem = ({ product, handleRemoveFromCart }) => {
+	const { id, img, name, price, quantity, shipping } = product;
+
+	return (
+		<div className="review-item">
+			<div className="item-details">
+				<img src={img} alt="" />
+				<div>
+					<h4>{name}</h4>
+					<p>
+						Price: <span className="orange-text">${price}</span>
+					</p>
+					<p>
+						Order Quantity: <span className="orange-text">{quantity}</span>
+					</p>
+				</div>
+			</div>
+			<button onClick={() => handleRemoveFromCart(id)}>
+				<FontAwesomeIcon icon={faTrashAlt} />
+			</button>
+		</div>
+	);
+};
+
+export default ReviewItem;
